477f4a0aa8d99dd20f411ae8f823b3febcd669c2
ZYAD626A <dhemzyad403@gmail.com>
2024-12-15 20:56:16 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+var zero=0;
+var numbers=document.getElementById('n1');
+
+function D(){
+    if(form1.num1.value<0){
+        alert('أقل من الصفر');
+
+    }
+    if(form1.num1.value>0){
+        alert('أكبر من الصفر');
+
+    }
+    if(form1.num1.value==0){
+        alert('تساوي صفر');
+
+    }
+    
+}
+
+
+
+
+
+
+
+
